45370eccbdbcf1625de8793f0fb0042b834c7dd7
Youssef <Youssef.Ahmed.719@h-eng.helwan.edu.eg>
2024-05-02 00:11:03 +0300

    fix some errors

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import Resume from "./components/resume/Resume";
 import Portoflio from "./components/portoflio/Portoflio";
 import Pricing from "./components/pricing/Pricing";
 import Testimonilas from "./components/testimonials/Testimonilas";
-import Blog from "./components/bolg/Blog";
 import Contact from "./components/contact/Contact";
 import { Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "./store/slices/alldata";
 
 function App() {
-	const state = useSelector((res) => res.data);
+	// const state = useSelector((res) => res.data);
 	// console.log(state);
 	const dispatch = useDispatch();
 	useEffect(() => {

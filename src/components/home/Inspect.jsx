c74023b2e33c11f2496002f8e55af2b84183322c
Youssef <Youssef.Ahmed.719@h-eng.helwan.edu.eg>
2024-05-02 00:45:42 +0300

    fix inspect

--- src/components/home/Inspect.jsx
+++ src/components/home/Inspect.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+
 const Inspect = ({ state }) => {
 	const mySwal = withReactContent(swal);
+
 	useEffect(() => {
-		if (state == "true") {
+		if (state === "true") {
 			function handleKeyDown(event) {
 				if (
 					(event.ctrlKey && event.keyCode === 67) || // Ctrl+C
@@ -13,19 +15,30 @@ const Inspect = ({ state }) => {
 					(event.metaKey && event.altKey && event.keyCode === 73) || // Cmd+Option+I (Mac)
 					(event.metaKey && event.altKey && event.keyCode === 67) // Cmd+Option+C (Mac)
 				) {
-					swal.fire({
-						title: "You can't you're not Joe!",
+					event.preventDefault(); // Prevent the default action
+					mySwal.fire({
+						title: "You can't, you're not Joe!",
 						icon: "error",
 						text: "This website is protected by the developer to prevent unauthorized access.",
 					});
-					event.preventDefault(); // Prevent the default action
 				}
 			}
 
+			function handleContextMenu(event) {
+				event.preventDefault(); // Prevent the context menu from appearing
+				mySwal.fire({
+					title: "You can't, you're not Joe!",
+					icon: "error",
+					text: "This website is protected by the developer to prevent unauthorized access.",
+				});
+			}
+
 			document.addEventListener("keydown", handleKeyDown);
+			document.addEventListener("contextmenu", handleContextMenu);
 
 			return () => {
 				document.removeEventListener("keydown", handleKeyDown);
+				document.removeEventListener("contextmenu", handleContextMenu);
 			};
 		}
 	}, []);

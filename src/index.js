bf1d087ac939c3053f01659e99bb299a28da47b5
Youssef <Youssef.Ahmed.719@h-eng.helwan.edu.eg>
2024-05-10 00:38:05 +0300

    pwa activate

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/index";
@@ -17,7 +18,7 @@ root.render(
 		</BrowserRouter>
 	</React.StrictMode>
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
